8903bea0bb9eda13e9c3a0abc97711d4f164a13c
Efio-esien Efiom <efio-esien@tangerine.africa>
2026-08-27 16:04:21 +0100

    updated conftest file

--- conftest.py
+++ conftest.py
@@ -1,7 +1,14 @@
 from playwright.sync_api import sync_playwright
 import pytest
+from dotenv import load_dotenv
 from pages.login_page import LoginPage
 import os
+
+
+
+@pytest.fixture(scope='session', autouse=True)
+def load_env():
+    load_dotenv()
 
 
 @pytest.fixture(scope='session')
